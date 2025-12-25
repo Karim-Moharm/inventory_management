18b766387bb7891ee1f5aa1f78bef8565c339dc7
karim-moharm <karimmoharm222@gmail.com>
2025-12-25 18:02:17 +0200

    auth using django builtin

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "inventory",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "config.urls"
+
+# crispy forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 TEMPLATES = [
     {
@@ -116,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/logged-out/"
+LOGIN_URL = "/login/"
--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
 from .views import TemplateView, SignUpView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", TemplateView.as_view(), name="dashboard"),
-    path("signup/", SignUpView.as_view(), name="sign_up"),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="inventory/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout",
+    ),
+    path(
+        "logged-out/",
+        TemplateView.as_view(template_name="inventory/logout.html"),
+        name="logged_out",
+    ),
 ]
